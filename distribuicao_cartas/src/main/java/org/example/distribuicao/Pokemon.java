90cf12a0954b1dd343c780bcc98560b0265e5c51
rick <henrique.campello@ges.inatel.br>
2025-10-28 18:17:01 -0300

    Delete distribuicao_cartas directory

--- distribuicao_cartas/src/main/java/org/example/distribuicao/Pokemon.java
+++
@@ -1,6 +0,0 @@
-package org.example.distribuicao;
-
-public class Pokemon {
-    private int numeroPokedex;
-    private String nome;
-}
--- distribuicao_cartas/src/main/java/org/example/distribuicao/StatusDistribuicao.java
+++
@@ -1,7 +0,0 @@
-package org.example.distribuicao;
-
-public class StatusDistribuicao {
-    private int id;
-    private String mensagemStatus;
-    private String log;
-}
