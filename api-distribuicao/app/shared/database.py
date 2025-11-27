9a29a56c1475ccb71df22a983eb484fc401dc49c
zSh3rl0cK <pedrohrdias12@gmail.com>
2025-11-27 17:41:03 -0300

    refactor: conexão com banco feita com variáveis de ambiente

--- api-distribuicao/app/shared/database.py
+++ api-distribuicao/app/shared/database.py
@@ -1,8 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
 
-DB_URL = 'mysql+pymysql://root:zSh3rl0cK$20@localhost:3306/distribuicao_de_cartas'
+load_dotenv()
+
+DB_USER = os.environ.get("DB_USER", "fallback_user")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "fallback_password")
+DB_HOST = os.environ.get("DB_HOST", "localhost")
+DB_PORT = os.environ.get("DB_PORT", "3306")
+DB_NAME = os.environ.get("DB_NAME", "distribuicao_de_cartas")
+
+DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DB_URL)
 
 Base = declarative_base()
